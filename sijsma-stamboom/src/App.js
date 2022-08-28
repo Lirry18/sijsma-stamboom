ffe34e73cb504f4725c224d0f9be72782a2134da
Lirry18 <lirry.pinter@gmail.com>
2022-08-28 17:17:10 +0200

    test

--- sijsma-stamboom/src/App.js
+++ sijsma-stamboom/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactFamilyTree from "react-family-tree";
+import nodes from "./data.json";
+import PinchZoomPan from "pinch-zoom-pan";
+import FamilyNode from "./FamilyNode";
+import styles from "./App.module.css";
 
-function App() {
+import "./styles.css";
+
+const myID = "0";
+const WIDTH = 260;
+const HEIGHT = 280;
+
+export default function App() {
+  const [rootId, setRootId] = React.useState(myID);
+  //const onResetClick = React.useCallback(() => setRootId(myID), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <PinchZoomPan
+          captureWheel
+          min={0.2}
+          max={2.5}
+          className={styles.wrapper}
+          key={rootId}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <ReactFamilyTree
+            nodes={nodes}
+            rootId={rootId}
+            width={WIDTH}
+            height={HEIGHT}
+            canvasClassName={styles.tree}
+            renderNode={node => (
+              <FamilyNode
+                key={node.id}
+                node={node}
+                isRoot={node.id === rootId}
+                onSubClick={setRootId}
+                style={{
+                  width: WIDTH,
+                  height: HEIGHT,
+                  transform: `translate(${node.left *
+                    (WIDTH / 2)}px, ${node.top * (HEIGHT / 2)}px)`
+                }}
+              />
+            )}
+          />
+        </PinchZoomPan>
   );
 }
-
-export default App;
